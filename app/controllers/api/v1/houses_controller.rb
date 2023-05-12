c08ba0988ba1f495a44a59128fcb1f8e883a76d7
Juanpa8830 <jp3_4@hotmail.com>
2023-05-12 15:33:26 -0500

    Add methods to  houses controller

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,6 +1,37 @@
 class Api::V1::HousesController < ApplicationController
+ 
+   # GET /api/v1/houses
   def index
     @houses = House.all
     render json: @houses
   end
+
+   # GET /api/v1/houses/:id
+  def show
+    @house = House.find(params[:id])
+    render json: @house
+  end
+
+
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      render json: @house
+    else
+      render json: @house.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @house = Bike.find(params[:id])
+    @house.destroy
+  end
+
+  private
+
+  def house_params
+    
+    params.require(:house).permit(:house_type, :photo, :garage, :bedrooms, :bathrooms, :area, :location, :daily_price, :description)
+  end
+
 end
