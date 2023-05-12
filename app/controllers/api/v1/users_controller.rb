7656c27ae03199631d85e30c38fb6eadc85bb11f
Juanpa8830 <jp3_4@hotmail.com>
2023-05-12 15:33:56 -0500

    Add methods to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,24 @@
 class Api::V1::UsersController < ApplicationController
+  
+  # get api/v1/users
   def index
     @users = User.all
     render json: @users
   end
+ 
+  # post api/v1/users => params should be on body
+  def create
+    user = User.new(user_params)
+    puts user
+    if user.save
+      render json: user, status: :ok
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+private
+def user_params
+  params.require(:user).permit(:name)
+end
 end
