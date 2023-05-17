e4bf55f571eb8b609f38d5d85dce9816a3d13d6c
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-05-17 14:52:32 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  post '/login', to: 'accounts#create'
+  post '/logout', to: 'accounts#destroy'
+  get "/reservations/:name", to: 'reservations#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create] do
-        resources :reservations, only: [:index, :show, :create, :destroy] 
-      end
-
+      resources :users, only: [:index, :create] 
+      resources :reservations, only: [:index, :show, :create, :destroy] 
       resources :houses, only: [:index, :show, :create, :destroy] 
-    end
+    end 
   end
-      
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
