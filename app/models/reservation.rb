569781ac3d29325e59c86d275db790c3a9bf0017
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-05-17 14:52:19 -0600

    Update validation to check startDate

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,12 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :house
 
-  validates :startDate, :endDate, presence: true
+  validates :user_id, :house_id, presence: true
+  validate :startDate_less_than_endDate
+
+  def startDate_less_than_endDate
+    return unless startDate && endDate && startDate > endDate
+
+    errors.add(:endDate, "endDate can't be earlier than startDate")
+  end
 end
