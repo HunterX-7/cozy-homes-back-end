4ef1f8f173ce703f0faf81adb2b76c469fb03aaf
Juanpa8830 <jp3_4@hotmail.com>
2023-05-24 12:56:50 -0500

    refacto api documentation

--- spec/requests/api/v1/accounts_spec.rb
+++ spec/requests/api/v1/accounts_spec.rb
@@ -1,7 +1,21 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe 'Api::V1::Accounts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'api/v1/accounts', type: :request do
+
+  path '/api/v1/accounts' do
+
+    post('create account') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
