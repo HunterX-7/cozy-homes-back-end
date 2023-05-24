9b77b1c09e9411ac88ed6ef3ab5f4dc5a1afc192
HunterX-7 <fraquin7@gmail.com>
2023-05-24 14:54:04 -0500

    Fix linters

--- spec/requests/api/v1/accounts_spec.rb
+++ spec/requests/api/v1/accounts_spec.rb
@@ -1,12 +1,9 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/accounts', type: :request do
-
   path '/api/v1/accounts' do
-
     post('create account') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
